a78fbf645089c9926dd3ed25bf31000e09236f3e
Kevin F Browne <kbrowneatx@gmail.com>
2013-04-15 13:56:01 -0500

    Added Ruby/Rails weblog base files

---
+++ Ruby/weblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weblog::Application.config.secret_token = 'e3c4174349746582193e80f01fd82306954d9568835b1f3295bccc594bce9fd7468ba05096623633526ff9d50844a3c6ab1f206e4a7a053d9360f5b7d3297df8'
